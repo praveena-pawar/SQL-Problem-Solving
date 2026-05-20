b16245381381e04e004b3db021b33d9af0048397
praveena-pawar <praveena.dev02@gmail.com>
2026-05-20 16:13:57 +0530

    updated

--- filtering_data.sql
+++ filtering_data.sql
@@ -1,21 +1,3 @@
-CREATE DATABASE interview_prep_db;
-
-USE interview_prep_db;
-
-CREATE TABLE employees(
-emp_id INT PRIMARY KEY,
-name VARCHAR(20),
-department VARCHAR(10),
-salary INT
-);
-
-INSERT INTO employees VALUES
-(1, "Alice", "HR", 45000),
-(2, "Bod", "IT", 70000),
-(3, "Charlie", "IT", 65000),
-(4, "David", "Finance", 55000),
-(5, "Eva", "IT", 80000);
-
 
 -- Add the manager_id column to your existing employees table
 ALTER TABLE employees
