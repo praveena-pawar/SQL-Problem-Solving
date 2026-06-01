2fabcd3b150d30306cbad3fa2ccd9186d4956c70
praveena-pawar <praveena.dev02@gmail.com>
2026-06-01 23:44:49 +0530

    solved query 34

--- filtering_data.sql
+++ filtering_data.sql
@@ -417,3 +417,11 @@ WHERE salary = (SELECT salary
 
 
 -- 34 — Department with Highest Average Salary
+SELECT d.department_name,
+       AVG(e.salary) AS average_salary
+FROM employees e
+JOIN departments d
+ON e.department_id = d.department_id
+GROUP BY d.department_name
+ORDER BY average_salary DESC
+LIMIT 1;
