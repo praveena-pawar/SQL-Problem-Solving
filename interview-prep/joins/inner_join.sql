e5597bfce5647cfe01ba387e84e8ea000c6fae2b
praveena-pawar <praveena.dev02@gmail.com>
2026-08-02 19:50:08 +0530

    query 3

--- interview-prep/joins/inner_join.sql
+++ interview-prep/joins/inner_join.sql
@@ -8,7 +8,7 @@ ON e.department_id = d.department_id
 ORDER BY department_name, emp_name;
 
 
--- 26: Requirements
+-- 2: Requirements
 -- Show only employees whose salary is greater than 55,000.
 -- Sort by:
 -- salary (descending)
@@ -19,3 +19,22 @@ INNER JOIN departments d
 ON e.department_id = d.department_id 
 WHERE e.salary > 55000
 ORDER BY e.salary DESC, emp_name;
+
+
+-- 3: Requirements
+-- Consider only employees whose salary is at least 50,000.
+-- average_salary should be rounded to 2 decimal places.
+-- Show only departments having at least 2 employees.
+-- Sort by:
+-- average_salary (descending)
+-- department_name (ascending)
+SELECT d.department_name, 
+	COUNT(e.emp_id) AS employee_count,
+    ROUND(AVG(e.salary), 2) AS average_salary
+FROM employees1 e
+INNER JOIN departments d
+ON e.department_id = d.department_id 
+WHERE e.salary >= 50000
+GROUP BY d.department_id, d.department_name
+HAVING COUNT(e.emp_id) >= 2
+ORDER BY average_salary DESC, department_name;
