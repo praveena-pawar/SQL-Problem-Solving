1ed019ec55cb04403aabe4f3c738a3b478c9b83d
praveena-pawar <praveena.dev02@gmail.com>
2026-08-02 19:49:39 +0530

    query 2

--- interview-prep/joins/inner_join.sql
+++ interview-prep/joins/inner_join.sql
@@ -6,3 +6,16 @@ FROM employees1 e
 INNER JOIN departments d
 ON e.department_id = d.department_id 
 ORDER BY department_name, emp_name;
+
+
+-- 26: Requirements
+-- Show only employees whose salary is greater than 55,000.
+-- Sort by:
+-- salary (descending)
+-- emp_name (ascending)
+SELECT e.emp_name, d.department_name, e.salary
+FROM employees1 e
+INNER JOIN departments d
+ON e.department_id = d.department_id 
+WHERE e.salary > 55000
+ORDER BY e.salary DESC, emp_name;
