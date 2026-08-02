9982a26c4d6e15c9e72c6dc7aa6409d19814d7bf
praveena-pawar <praveena.dev02@gmail.com>
2026-08-02 20:18:52 +0530

    query 5

--- interview-prep/joins/inner_join.sql
+++ interview-prep/joins/inner_join.sql
@@ -40,7 +40,7 @@ HAVING COUNT(e.emp_id) >= 2
 ORDER BY average_salary DESC, department_name;
 
 
--- 28: Requirements
+-- 4: Requirements
 -- Show only employees who:
 -- belong to Engineering or HR.
 -- were hired on or after '2021-01-01'.
@@ -57,3 +57,25 @@ WHERE d.department_name IN ('Engineering', 'HR')
   AND e.hire_date >= '2021-01-01'
   AND e.salary >= 55000
 ORDER BY d.department_name, e.salary DESC, e.emp_name;
+
+
+-- 29: Requirements
+-- Consider only employees:
+-- hired before '2023-01-01'
+-- with salary at least 50,000
+-- Show only departments where:
+-- employee_count is at least 2
+-- total_salary is greater than 120000
+SELECT d.department_name, 
+	COUNT(e.emp_id) AS employee_count,
+    SUM(e.salary) AS total_salary,
+    MAX(e.salary) AS highest_salary
+FROM employees1 e
+INNER JOIN departments d
+ON e.department_id = d.department_id
+WHERE hire_date < '2023-01-01'
+	AND salary >= 50000
+GROUP BY d.department_id
+HAVING COUNT(e.emp_id) >= 2
+	AND SUM(e.salary) > 120000
+ORDER BY total_salary DESC, d.department_name;
