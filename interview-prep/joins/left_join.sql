f0e43f6029fb54fcd6cd5d9acc147dcb7d6db852
praveena-pawar <praveena.dev02@gmail.com>
2026-08-06 19:20:29 +0530

    query 6

--- interview-prep/joins/left_join.sql
+++ interview-prep/joins/left_join.sql
@@ -74,3 +74,15 @@ GROUP BY d.department_id, d.department_name
 HAVING COUNT(e.emp_id) = 0 OR 
 		COUNT(e.emp_id) >= 2
 ORDER BY employee_count DESC, d.department_name;
+
+
+
+-- 36: Requirements
+-- Show all departments, even if they have no employees.
+-- Consider only employees whose salary is greater than or equal to 50,000.
+-- Show only departments where:
+-- employee_count is 0 or at least 2.
+-- Use the aliases:
+-- employee_count
+-- total_salary
+-- highest_salary
