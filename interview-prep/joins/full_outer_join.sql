a9c38807e7e233bf9570e267621db7704300410f
praveena-pawar <praveena.dev02@gmail.com>
2026-08-08 17:29:54 +0530

    solved query 1

--- interview-prep/joins/full_outer_join.sql
+++ interview-prep/joins/full_outer_join.sql
@@ -5,3 +5,13 @@
 -- all employees,
 -- all departments,
 -- even if there is no matching record. 
+SELECT e.emp_name, d.department_name
+FROM employees1 e
+LEFT JOIN departments d
+ON e.department_id = d.department_id
+UNION 
+SELECT e.emp_name, d.department_name
+FROM employees1 e
+RIGHT JOIN departments d
+ON e.department_id = d.department_id
+ORDER BY department_name, emp_name;
