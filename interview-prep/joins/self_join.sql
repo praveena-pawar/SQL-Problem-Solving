c9465db7e8ec541150ef573b9d9b37f7bffc0690
praveena-pawar <praveena.dev02@gmail.com>
2026-08-08 17:33:21 +0530

    solved query 1

--- interview-prep/joins/self_join.sql
+++ interview-prep/joins/self_join.sql
@@ -4,3 +4,10 @@
 -- Use these aliases:
 -- employee_name
 -- manager_name
+SELECT
+    e.emp_name AS employee_name,
+    m.emp_name AS manager_name
+FROM employees2 e
+JOIN employees2 m
+    ON e.manager_id = m.emp_id
+ORDER BY employee_name;
