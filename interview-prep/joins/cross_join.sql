1b2edc92318a5b6c04f840e3ac264724122f6e6a
praveena-pawar <praveena.dev02@gmail.com>
2026-08-08 17:37:04 +0530

    solved query 31

--- interview-prep/joins/cross_join.sql
+++ interview-prep/joins/cross_join.sql
@@ -2,3 +2,7 @@
 -- e.emp_name
 -- d.department_name
 -- Return every possible combination of employees and departments.
+SELECT e.emp_name, d.department_name
+FROM employees1 e
+CROSS JOIN departments d
+ORDER BY e.emp_name, d.department_name;
