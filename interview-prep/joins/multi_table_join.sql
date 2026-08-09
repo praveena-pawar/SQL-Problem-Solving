39578221ba499356cfa0d702ec91bd73ea62a645
praveena-pawar <praveena.dev02@gmail.com>
2026-08-09 22:39:17 +0530

    solved query 2

--- interview-prep/joins/multi_table_join.sql
+++ interview-prep/joins/multi_table_join.sql
@@ -17,3 +17,15 @@ ORDER BY city, emp_name;
 -- belong to Engineering or HR
 -- have salary at least 55,000
 -- work in India
+SELECT e.emp_name, d.department_name, l.city, e.salary
+FROM employees1 e
+INNER JOIN departments d
+ON e.department_id = d.department_id
+INNER JOIN locations l
+ON l.department_id = d.department_id
+WHERE d.department_name IN ('Engineering', 'HR')
+		AND 
+        e.salary >= 55000
+        AND 
+        country = 'India'
+ORDER BY d.department_name, e.salary DESC, e.emp_name;
