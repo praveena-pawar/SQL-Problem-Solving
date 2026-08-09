b34bfb02c3630787098a756f60d48c5b64e7c4f6
praveena-pawar <praveena.dev02@gmail.com>
2026-08-09 22:38:16 +0530

    solved query 1

--- interview-prep/joins/multi_table_join.sql
+++ interview-prep/joins/multi_table_join.sql
@@ -3,3 +3,10 @@
 -- Sort by:
 -- city ascending
 -- emp_name ascending
+SELECT e.emp_name, d.department_name, l.city
+FROM employees1 e
+INNER JOIN departments d
+ON e.department_id = d.department_id
+INNER JOIN locations l
+ON l.department_id = d.department_id
+ORDER BY city, emp_name;
