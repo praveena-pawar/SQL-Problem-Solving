3e2694f9eddb7432cef36de50aaa8349736902c9
praveena-pawar <praveena.dev02@gmail.com>
2026-08-09 22:38:41 +0530

    query 2

--- interview-prep/joins/multi_table_join.sql
+++ interview-prep/joins/multi_table_join.sql
@@ -10,3 +10,10 @@ ON e.department_id = d.department_id
 INNER JOIN locations l
 ON l.department_id = d.department_id
 ORDER BY city, emp_name;
+
+
+-- 2: Requirements
+-- Show only employees who:
+-- belong to Engineering or HR
+-- have salary at least 55,000
+-- work in India
