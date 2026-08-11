cb285e74e86e0d199edc2f232aee39f163f23dc0
praveena-pawar <praveena.dev02@gmail.com>
2026-08-11 21:46:39 +0530

    solved 5 problems

--- interview-prep/joins/multi_table_join.sql
+++ interview-prep/joins/multi_table_join.sql
@@ -12,6 +12,7 @@ ON l.department_id = d.department_id
 ORDER BY city, emp_name;
 
 
+
 -- 2: Requirements
 -- Show only employees who:
 -- belong to Engineering or HR
@@ -29,6 +30,7 @@ WHERE d.department_name IN ('Engineering', 'HR')
         AND 
         country = 'India'
 ORDER BY d.department_name, e.salary DESC, e.emp_name;
+
 
 
 -- 3: Requirements
