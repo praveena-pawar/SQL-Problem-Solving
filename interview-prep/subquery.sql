7a38b4b61eaa3beb554605e699716228183968dd
praveena-pawar <praveena.dev02@gmail.com>
2026-09-25 22:25:02 +0530

    solved query 17

--- interview-prep/subquery.sql
+++ interview-prep/subquery.sql
@@ -375,5 +375,14 @@ WHERE eo.order_count > ALL (
 -- emp_name
 -- department_id
 -- salary
-
-
+SELECT
+    e.emp_name,
+    e.department_id,
+    e.salary
+FROM employees1 e
+WHERE e.salary > ALL (
+    SELECT e2.salary
+    FROM employees1 e2
+    WHERE e2.department_id = e.department_id
+      AND e2.emp_id <> e.emp_id
+);
